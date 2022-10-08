6fc233a06641e6cc03d00706172c9881c6a44b65
JustMarfix <justmarfix@vk.com>
2022-10-08 09:42:23 +0300

    Ну теперь точно последний баг

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import mariadb
 import telebot
 import schedule
+from io import BytesIO
+import requests
 
 from first_config import Config
-from cats import send_cat, send_all
 
 bot = telebot.TeleBot(Config.token)
 
@@ -16,6 +17,24 @@ conn = mariadb.connect(
 )
 
 cursor = conn.cursor()
+
+def send_cat(chat_id):
+    with BytesIO(requests.get(requests.get(Config.url).json()[0]['url']).content) as photo:
+        bot.send_photo(chat_id, photo)
+
+def send_all(h):
+    try:
+        global cursor
+        cursor.execute(f"SELECT cid FROM `users` WHERE time = '{h}'")
+        users = cursor.fetchall()
+        
+        for i in users:
+            send_cat(users[i][0])
+    except mariadb.Error as e:
+        print(f"Ошибка при рассылке людям с таймером {h}:\n {e}")
+
+    print('Рассылка успешно завершена.')
+
 
 @bot.message_handler(content_types=['text'])
 def new_message(message):
